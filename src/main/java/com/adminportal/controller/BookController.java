36c659a949a07265a1321a2b479314e09c037aae
huyquang2710 <huyquang271099@gmail.com>
2023-07-04 14:03:01 +0700

    ADD BOOK LOGIC

--- src/main/java/com/adminportal/controller/BookController.java
+++ src/main/java/com/adminportal/controller/BookController.java
@@ -1,21 +1,64 @@
 package com.adminportal.controller;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.adminportal.domain.Book;
+import com.adminportal.service.BookService;
 
 @Controller
 @RequestMapping("/book")
 public class BookController {
-	
+
+	@Autowired
+	private BookService bookService;
+
 	@GetMapping("/add")
-	public String addBook(Model model) {
+	public String addBookGet(Model model) {
 		Book book = new Book();
 		model.addAttribute("book", book);
-		
+
 		return "addBook";
+	}
+
+	@PostMapping("/add")
+	public String addBookPost(Model model, @ModelAttribute("book") Book book, HttpServletRequest request) {
+
+		bookService.save(book);
+
+		MultipartFile bookImage = book.getBookImage();
+
+		try {
+			byte[] bytes = bookImage.getBytes();
+			String name = book.getId() + ".png";
+			BufferedOutputStream stream = new BufferedOutputStream(
+					new FileOutputStream(new File("src/main/resources/static/images/book/" + name)));
+			stream.write(bytes);
+			stream.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		model.addAttribute("book", book);
+
+		return "redirect:bookList";
+	}
+
+	@GetMapping("/bookList")
+	public String bookList(Model model) {
+
+		return "bookList";
 	}
 }
