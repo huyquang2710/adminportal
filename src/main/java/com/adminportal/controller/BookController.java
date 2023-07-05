5d2420d7f6630ef879961e623cd728256b1a3d97
huyquang2710 <huyquang271099@gmail.com>
2023-07-05 17:37:31 +0700

    UPDATE BOOKLOGIC

--- src/main/java/com/adminportal/controller/BookController.java
+++ src/main/java/com/adminportal/controller/BookController.java
@@ -3,6 +3,8 @@ package com.adminportal.controller;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -74,10 +76,31 @@ public class BookController {
 	}
 
 	@GetMapping("/updateBook")
-	public String updateBook(@RequestParam("id") Long id, Model model) {
+	public String updateBookGet(@RequestParam("id") Long id, Model model) {
 		Book book = bookService.fineOne(id);
 
 		model.addAttribute("book", book);
 		return "updateBook";
+	}
+
+	@PostMapping("/updateBook")
+	public String updateBookPost(@ModelAttribute("book") Book book, Model model, HttpServletRequest request) {
+		bookService.save(book);
+		MultipartFile bookImage = book.getBookImage();
+		if (bookImage.isEmpty()) {
+			try {
+				byte[] bytes = bookImage.getBytes();
+				String name = book.getId() + ".png";
+				Files.delete(Paths.get("src/main/resources/static/images/book" + name));
+
+				BufferedOutputStream stream = new BufferedOutputStream(
+						new FileOutputStream(new File("src/main/resources/static/images/book" + name)));
+				stream.write(bytes);
+				stream.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return "redirect:/book/bookInfo?id=" + book.getId();
 	}
 }
